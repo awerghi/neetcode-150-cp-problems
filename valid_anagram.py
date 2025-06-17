67f76cb1a578846b6c989bc20b3ffafb8e174838
WERGHI Ahmed <ahmed.werghi@oscaro.com>
2025-06-17 23:59:30 +0200

    valid-anagram: two separate solution with different time complexities

---
+++ valid_anagram.py
@@ -0,0 +1,45 @@
+
+# solution 1
+# sort both strings and compare
+# Time complexity = O(n log(n))
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+s = Solution()
+print(s.isAnagram("jar","jam")) # False
+print(s.isAnagram("racecar","racecar")) # True
+
+# solution 2:
+# make two tables of 26, number of alphabetic carachters,
+# compare them after filling them
+# Time complexity = O(n)
+
+class OptimalSolution:
+
+    def fill_table_with_string (self, s : str):
+        alpha_s = [0 for i in range(26)]
+        for i in range(len(s)):
+            alpha_s[ord(s[i])-ord('a')] += 1
+
+        return alpha_s
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        alpha_s = self.fill_table_with_string(s)
+        alpha_t = self.fill_table_with_string(t)
+
+        for i in range(26):
+            if alpha_t[i] == alpha_s[i]:
+                continue
+            else:
+                return False
+
+        return True
+
+
+
+s = OptimalSolution()
+print(s.isAnagram("jar","jam")) # False
+print(s.isAnagram("racecar","racecar")) # True
+
