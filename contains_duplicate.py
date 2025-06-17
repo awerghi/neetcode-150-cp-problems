7de4d019eee76ad7bbd52a0b41520376d68750cc
WERGHI Ahmed <ahmed.werghi@oscaro.com>
2025-06-18 00:18:56 +0200

    neetcode : contains_duplicate problem solution

---
+++ contains_duplicate.py
@@ -0,0 +1,20 @@
+# Problem statement : https://neetcode.io/problems/duplicate-integer?list=neetcode150
+
+from typing import List
+
+class Solution:
+
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        map_occ = {}
+        for elt in nums:
+            if map_occ.get(elt) is not None:
+                return True
+            else:
+                map_occ[elt] = 1
+
+        return False
+
+s = Solution()
+
+print(s.hasDuplicate([1, 2, 3, 3])) # -> True
+print(s.hasDuplicate([1, 2, 3, 4])) # -> False
