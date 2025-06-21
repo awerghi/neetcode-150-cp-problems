67c66dbcf0e07d79a33ed30d29d226d114a18086
WERGHI Ahmed <ahmed.werghi@oscaro.com>
2025-06-22 01:18:23 +0200

    two_integer_sum problem solution

---
+++ two-pointers/two_integer_sum.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) - 1
+        while i < j:
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                i += 1
+
+        return [-1]
+
+
+s = Solution()
+print(s.twoSum([1,2,3,4],3))
