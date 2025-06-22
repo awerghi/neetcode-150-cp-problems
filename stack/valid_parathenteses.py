9b9fcaa5543efcc68ae2a146c99e15d8ad1d842f
WERGHI Ahmed <ahmed.werghi@oscaro.com>
2025-06-22 12:54:45 +0200

    valid_parathenteses problem solution

---
+++ stack/valid_parathenteses.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        complement = {"{" : "}","[" : "]", "(" : ")"}
+        right_direction = ["}","]",")"]
+        stack_l = []
+        stack_r = []
+        for elt in s:
+            stack_l.append(elt)
+
+        while stack_l:
+            current = stack_l.pop()
+            if current in right_direction:
+                stack_r.append(current)
+            else :
+                current_r = stack_r.pop() if stack_r else ""
+                if complement.get(current) != current_r:
+                    return False
+                else:
+                    continue
+        if len(stack_r) == 0 and len(stack_l) == 0:
+            return True
+        else:
+            return False
+
+
+s = Solution()
+print(s.isValid("]"))
