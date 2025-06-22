bc71f04a83a434810997af5f3a590aaf0c4645f9
WERGHI Ahmed <ahmed.werghi@oscaro.com>
2025-06-22 12:27:20 +0200

    add 3sum problem solution

---
+++ two-pointers/3sum.py
@@ -0,0 +1,42 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[List[int]]:
+        i = 0
+        j = len(numbers) - 1
+        res_two_sum = []
+        while i < j:
+            if numbers[i] + numbers[j] == target:
+                res_two_sum.append([numbers[i], numbers[j]])
+                i += 1
+                j -= 1
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                i += 1
+        if res_two_sum:
+            return res_two_sum
+        return [[-1]]
+
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        three_sum_result = []
+        for i in range(len(nums)):
+            # get the list from current index + 1 in order to not get duplicates
+            new_nums = nums[i+1:]
+            new_nums.sort()
+            # search for the negative of the current number using two sum feature
+            res_new_nums = self.twoSum(new_nums,-1 * nums[i])
+            if res_new_nums == [[-1]]:
+                continue
+            else :
+                for elt in res_new_nums:
+                    elt.append(nums[i])
+                    elt.sort()
+                    if elt not in three_sum_result:
+                        three_sum_result.append(elt)
+        return three_sum_result
+
+
+s = Solution()
+print(s.threeSum([-2,0,1,1,2]))
