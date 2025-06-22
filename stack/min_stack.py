85902682c19bcd68e449490d75237da7a0516f91
WERGHI Ahmed <ahmed.werghi@oscaro.com>
2025-06-22 13:16:20 +0200

    add min_stack pb solution

---
+++ stack/min_stack.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if len(self.stack) > 0:
+            self.stack.pop()
+
+
+    def top(self) -> int:
+        if len(self.stack) > 0:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        mini = float('inf')
+        aux_stack = self.stack.copy()
+        while aux_stack:
+            current = aux_stack.pop()
+            if current < mini :
+                mini = current
+        return mini
+
+my_stack = MinStack()
+my_stack.push(1)
+my_stack.push(2)
+my_stack.push(0)
+my_stack.getMin()
+my_stack.pop()
+my_stack.top()
+my_stack.getMin()
