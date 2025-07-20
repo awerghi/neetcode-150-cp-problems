76711bba9ab6cbe4786fe59c005a595093bef038
“awerghi” <ahmedwerghi@Mac.lan>
2025-07-20 21:26:38 +0200

    minimun in a rotated sorted array

---
+++ binary-search/find_minimum_rotated_sorted_array.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        start, end = 0, len(nums) - 1
+
+        while start < end:
+            mid = (start + end) // 2
+
+            if nums[mid] > nums[end]:
+                # Minimum is in the right half
+                start = mid + 1
+            else:
+                # Minimum is in the left half (including mid)
+                end = mid
+
+        return nums[start]
+
+
+
+s = Solution()
+print(s.findMin([2,1]))
