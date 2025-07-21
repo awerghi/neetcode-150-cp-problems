ef7293dade8a0c7c6adad7dc0d8fd8ffd606e013
“awerghi” <ahmedwerghi@Mac.lan>
2025-07-21 23:43:33 +0200

    add hashset class definition

---
+++ hash_set.py
@@ -0,0 +1,30 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.hashset = []
+
+    def add(self, key: int) -> None:
+        if not self.contains(key):
+            self.hashset.append(key)
+
+    def remove(self, key: int) -> None:
+        if self.contains(key):
+            self.hashset.remove(key)
+
+    def contains(self, key: int) -> bool:
+        for i in range(len(self.hashset)):
+            if self.hashset[i] == key :
+                return True
+        return False
+
+obj = MyHashSet()
+obj.add(1)
+obj.add(2)
+obj.add(3)
+print(obj.hashset)
+obj.remove(4)
+print(obj.hashset)
+obj.remove(1)
+print(obj.hashset)
+print(obj.contains(3))
+print(obj.contains(5))
