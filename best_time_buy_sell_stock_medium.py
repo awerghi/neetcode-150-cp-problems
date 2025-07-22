51d2db0683d856dcb0b2434de4acf608dc378a4a
“awerghi” <ahmedwerghi@Mac.lan>
2025-07-23 00:37:57 +0200

    buy sell stock version 2 solution

---
+++ best_time_buy_sell_stock_medium.py
@@ -0,0 +1,25 @@
+# Author ahmed.werghi@gmail.com
+# problem statement : https://neetcode.io/problems/best-time-to-buy-and-sell-stock-ii?list=neetcode250
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        sell = prices[0]
+        profit = 0
+
+        for i in range(1,len(prices)):
+            if prices[i] < buy :
+                buy = prices[i]
+                sell = prices[i]
+            if prices[i] > sell :
+                sell = prices[i]
+                profit += sell - buy
+                buy = prices[i]
+        return profit
+
+
+s = Solution()
+print(s.maxProfit([1,2,3,4,5])) # output : 4
+print(s.maxProfit([7,1,5,3,6,4])) # output : 7
