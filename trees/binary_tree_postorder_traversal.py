df0812ec160069f986423de8c43fd2d6671eae49
“awerghi” <ahmedwerghi@Mac.lan>
2025-07-30 12:22:17 +0200

    binary tree postorder traversal

---
+++ trees/binary_tree_postorder_traversal.py
@@ -0,0 +1,35 @@
+# Author ahmed.werghi@gmail.com
+# problem statement : https://neetcode.io/problems/binary-tree-postorder-traversal?list=neetcode250
+
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None :
+            return []
+
+        left = self.postorderTraversal(root.left)
+        right = self.postorderTraversal(root.right)
+
+        return left + right + [root.val]
+
+
+s = Solution()
+n4 = TreeNode(4)
+n5 = TreeNode(5)
+n6 = TreeNode(6)
+n7 = TreeNode(7)
+n2 = TreeNode(2,n4,n5)
+n3 = TreeNode(3,n6,n7)
+n1 = TreeNode(1,n2,n3)
+
+print(s.postorderTraversal(n1)) # output => [4,5,2,6,7,3,1]
+
