116c7d4fff1cbe380d7896ea851974c633e18778
“awerghi” <ahmedwerghi@Mac.lan>
2025-07-30 12:31:49 +0200

    invert binary tree pb solution

---
+++ trees/invert_binary_tree.py
@@ -0,0 +1,26 @@
+# Author ahmed.werghi@gmail.com
+# problem statement : https://neetcode.io/problems/invert-a-binary-tree?list=neetcode250
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if root is None:
+            return None
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        aux = root.left
+        root.left = root.right
+        root.right = aux
+
+        return root
