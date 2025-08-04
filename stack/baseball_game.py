69da7f6d07b0a40315f9fa52134be5f593f8a844
“awerghi” <ahmedwerghi@Mac.lan>
2025-08-04 22:50:18 +0200

    baseball game pb solution

---
+++ stack/baseball_game.py
@@ -0,0 +1,19 @@
+# PROBLEM STATEMENT : https://neetcode.io/problems/baseball-game?list=neetcode250
+# Author aw.ahmed.werghi@gmail.com
+
+from typing import List
+
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        scores = []
+        for i in range(len(operations)):
+            if operations[i] == "+":
+                scores.append(scores[-1] + scores[-2])
+            elif operations[i] == "C":
+                scores.pop()
+            elif operations[i] == "D":
+                scores.append(scores[-1] * 2)
+            else:
+                scores.append(int(operations[i]))
+            print(scores)
+        return sum(scores)
