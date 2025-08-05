821b9abfaf90eff78136e942a4ca3eba52530d7d
“awerghi” <ahmedwerghi@Mac.lan>
2025-08-05 13:47:06 +0200

    flip invert pb solution

---
+++ other-leetcode/flip_image.py
@@ -0,0 +1,15 @@
+# Author ahmed.werghi@gmail.com
+# problem statement : https://leetcode.com/problems/flipping-an-image/description/
+
+from typing import List
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        for elt in image:
+            elt.reverse()
+            for i in range(len(elt)):
+                elt[i] = 0 if elt[i] == 1 else 1
+        return image
+
+s = Solution()
+print(s.flipAndInvertImage([[1,1,0],[1,0,1],[0,0,0]])) # output : [[1,0,0],[0,1,0],[1,1,1]]
