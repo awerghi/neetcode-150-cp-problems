d9b865baddb0c8b7868f82a8b34055452a622bd4
“awerghi” <ahmedwerghi@Mac.lan>
2025-08-10 23:42:56 +0200

    level order traversal pb solution

---
+++ trees/level_order.py
@@ -0,0 +1,27 @@
+# Author ahmed.werghi@gmail.com
+# problem statement : https://neetcode.io/problems/level-order-traversal-of-binary-tree?list=neetcode250
+
+from typing import Optional, List
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root : return []
+        current = [root]
+        res = [[root.val]]
+        while current:
+            aux = []
+            for elt in current:
+                if elt.left is not None : aux.append(elt.left)
+                if elt.right is not None : aux.append(elt.right)
+            current = aux
+            current_vals = [elt.val for elt in current]
+            if current_vals : res.append(current_vals)
+
+        return res
