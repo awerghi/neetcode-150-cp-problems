c63059280e97e273f4243dee77b0b725a0e5b082
“awerghi” <ahmedwerghi@Mac.lan>
2025-08-12 00:09:57 +0200

    kth largest element in an array

---
+++ heap/kth_largest.py
@@ -0,0 +1,21 @@
+# Author ahmed.werghi@gmail.com
+# problem statement : https://neetcode.io/problems/kth-largest-element-in-an-array?list=neetcode250
+
+import heapq
+from typing import List
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heapq.heapify(nums)
+        n = len(nums) - k
+        current = 0
+        while n > 0 :
+            current = heapq.heappop(nums)
+            n -= 1
+
+        return current
+
+s = Solution()
+print(s.findKthLargest([3,2,1,5,6,4],2)) # output : 5
+print(s.findKthLargest([2,3,1,5,4],2)) # output : 4
+print(s.findKthLargest([2,3,1,1,5,5,4],3)) # output : 4
